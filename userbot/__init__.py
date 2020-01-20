853b8a164f1edb7ab4dd4b62d0aeccc82640d1e9
animeking9901 <tbradleyfb@gmail.com>
2020-01-20 16:09:03 -0500

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -40,12 +40,7 @@ if version_info[0] < 3 or version_info[1] < 6:
 
 # Check if the config was edited by using the already used variable.
 # Basically, its the 'virginity check' for the config file ;)
-CONFIG_CHECK = os.environ.get(
-
-if CONFIG_CHECK:
-    LOGS.info(
-        "Please remove the line mentioned in the first hashtag from the config.env file"
-    )
+if CONFIG_CHECK: LOGS.info("Please remove the line mentioned in the first hashtag from the config.env file)
     quit(1)
 
 # Telegram App KEY and HASH
